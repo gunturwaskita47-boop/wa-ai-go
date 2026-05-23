e516bfe35c6744975f677d62ff1de9759af304d1
gunturwaskita47-boop <gunturwaskita47@gmail.com>
2026-05-23 18:11:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10000"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "WhatsApp AI Bot Online")
+	})
+
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Webhook Active")
+	})
+
+	fmt.Println("Server running on port", port)
+	http.ListenAndServe(":"+port, nil)
+}
